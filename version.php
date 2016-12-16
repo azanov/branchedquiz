23de999a2ef64f99c7e30033a31f28b59fbdd2a4
Pavel Azanov <pavel@azanov.de>
2016-12-16 13:11:26 +0100

    chore(project): update version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_testflow';
-$plugin->version = 2016112601;
+$plugin->version = 2016121601;
 $plugin->release = 'v0.0.1';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
