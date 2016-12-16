146b34afd8ddd490635f41b22bc33c602258ce7b
Pavel Azanov <pavel@azanov.de>
2016-12-16 14:06:59 +0100

    up

--- behaviour.php
+++ behaviour.php
@@ -24,19 +24,13 @@
  * @copyright  2016 Your Name <your@email.address>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
+
 defined('MOODLE_INTERNAL') || die();
 
 class qbehaviour_testflow extends question_behaviour {
 
-    // public function __construct(question_attempt $qa, $preferredbehaviour) {
-    //     parent::__construct($qa, $preferredbehaviour);
-    //     $this->preferredbehaviour = $preferredbehaviour;
-    // }
-
     public function is_compatible_question(question_definition $question) {
         return true;
     }
-
 
 }
