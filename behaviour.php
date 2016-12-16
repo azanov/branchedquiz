3d2b00c4c015df6c62559a9bb8c5bbcbfb8413ab
Pavel Azanov <pavel@azanov.de>
2016-12-16 13:20:07 +0100

    fix(plugin): fix reference

--- behaviour.php
+++ behaviour.php
@@ -2,4 +2,13 @@ defined('MOODLE_INTERNAL') || die();
 
 class qbehaviour_testflow extends question_behaviour {
 
+	public function __construct(question_attempt $qa, $preferredbehaviour) {
+        parent::__construct($qa, $preferredbehaviour);
+        $this->preferredbehaviour = $preferredbehaviour;
+    }
+
+    public function is_compatible_question(question_definition $question) {
+        return true;
+    }
+
 }
