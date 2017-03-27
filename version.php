1156021b934efde0e9f20a883affc756e7cdf5d2
Pavel Azanov <pavel.azanov@pb.com>
2017-03-28 00:29:24 +0200

    refactor(styles): fix lint errors and do not include the styles twice

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017032703;
+$plugin->version   = 2017032704;
 $plugin->requires  = 2016051900;
 $plugin->component = 'mod_branchedquiz';
 $plugin->cron      = 60;
